1deaef0d8fdd1d78d8bf9fa7b2c696f2b87efc9f
Alec German <alec_german@yahoo.com>
2018-03-17 16:12:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,7 +38,9 @@ class InteractiveRecord
     values = []
 
     self.class.column_names.each do |col_name|
-
+      values << "'#{send(col_name)}'" unles send(col_name).nil?
     end
+
+    values
   end
 end
