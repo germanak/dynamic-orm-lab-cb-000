b925a73796291375434163e328abfdbba244cb01
Alec German <alec_german@yahoo.com>
2018-03-17 16:23:20 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,9 +58,11 @@ class InteractiveRecord
 
   def self.find_by(options={})
     result = []
-    option.each do |property, value|
+
+    options.each do |property, value|
       sql = "SELECT * FROM #{self.table_name} WHERE #{property} = #{value}"
     end
+
     result << DB[:conn].execute(sql)
   end
 end
